b0fa1bac94b1846177d5f8a36bcf950045aafac7
pietupai <pietupai@gmail.com>
2025-04-18 08:13:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ let gameRooms = {};
 app.post('/create-room', (req, res) => {
     const roomCode = Math.random().toString(36).substring(2, 8).toUpperCase();
     gameRooms[roomCode] = { players: [] };
-    res.json({ roomCode });
+    res.json({ success: true, roomCode });
 });
 
 app.post('/join-room', (req, res) => {
@@ -18,11 +18,22 @@ app.post('/join-room', (req, res) => {
 
     if (gameRooms[roomCode]) {
         gameRooms[roomCode].players.push(playerName);
+        res.json({ success: true });
+    } else {
+        res.status(404).json({ success: false, message: 'Room not found' });
+    }
+});
+
+app.get('/room/:roomCode', (req, res) => {
+    const roomCode = req.params.roomCode;
+
+    if (gameRooms[roomCode]) {
         res.json({ success: true, players: gameRooms[roomCode].players });
     } else {
         res.status(404).json({ success: false, message: 'Room not found' });
     }
 });
+
 
 
 app.listen(10000, () => console.log('Server running on port 10000'));
